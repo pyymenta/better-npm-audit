37a8b3adf0a382c585a6e181d4e9b9a7fabc2886
Lucas Pimenta <lucas.pimenta94@outlook.com>
2024-10-25 12:36:36 -0300

    add JSDOC to nsprc methods

--- src/utils/nsprc.ts
+++ src/utils/nsprc.ts
@@ -1,11 +1,21 @@
-import { NsprcFile, ReadNsprcStrategy } from "src/types";
-import { readFile } from "./file";
-import { ConfigFile } from "src/types/configFile";
+import { NsprcFile, ReadNsprcStrategy } from 'src/types';
+import { readFile } from './file';
+import { ConfigFile } from 'src/types/configFile';
 
+/**
+ * Strategy to get nsp config from nsprc file
+ * @param  {String} filePath File path
+ * @return {NsprcFile} The npm version
+ */
 export function fromNsprcFile(filePath: string): NsprcFile | boolean {
   return readFile(filePath);
 }
 
+/**
+ * Strategy to get nsp config from config requestNsprcFile function
+ * @param  {String} filePath File path
+ * @return {NsprcFile} The npm version
+ */
 export async function fromConfigFile(filePath: string): Promise<NsprcFile | boolean> {
   const configFileModule = (await import(`${process.cwd()}/${filePath}`)) as ConfigFile;
 
@@ -16,11 +26,22 @@ export async function fromConfigFile(filePath: string): Promise<NsprcFile | bool
   }
 }
 
-export async function nsprcReaderContext(strategy: ReadNsprcStrategy, filePath: string) {
+/**
+ * Returns strategy from context
+ * @param  {ReadNsprcStrategy} strategy File path
+ * @param  {String} filePath File path
+ * @return {ReadNsprcStrategy} Nsp config strategy
+ */
+export async function nsprcReaderContext(strategy: ReadNsprcStrategy, filePath: string): Promise<NsprcFile | boolean> {
   return await strategy(filePath);
 }
 
-export function nsprcReader(filePath: string): Function {
+/**
+ * Get nsp Reader
+ * @param  {String} filePath File path
+ * @return {ReadNsprcStrategy} Reader strategy
+ */
+export function nsprcReader(filePath: string): ReadNsprcStrategy {
   const ext = filePath.split('.').pop();
 
   if (ext === 'nsprc') {
